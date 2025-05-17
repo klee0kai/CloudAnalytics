161efb2e92faa49ae7f171276e9f61e2fc0d273f
klee0kai <klee0kai@gmail.com>
2025-05-17 22:49:22 +0200

    desktop test

--- composeApp/src/commonMain/kotlin/com/github/klee0kai/cloud/App.kt
+++ composeApp/src/commonMain/kotlin/com/github/klee0kai/cloud/App.kt
@@ -1,18 +1,19 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.github.klee0kai.cloud
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import kotlinproject.composeapp.generated.resources.Res
 import kotlinproject.composeapp.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.painterResource
@@ -22,21 +23,59 @@ import org.jetbrains.compose.resources.painterResource
 fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .background(color = Color.Gray.copy(alpha = 0.1f)),
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+
+
+            Column(
+                modifier = Modifier
+                    .safeContentPadding()
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Button(onClick = { showContent = !showContent }) {
+                    Text("Click me!")
                 }
+                AnimatedVisibility(showContent) {
+                    val greeting = remember { Greeting().greet() }
+                    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                        Image(painterResource(Res.drawable.compose_multiplatform), null)
+                        Text("Compose: $greeting")
+                    }
+                }
+            }
+
+            TopAppBar(
+                modifier = Modifier,
+                title = {
+
+                },
+                navigationIcon = {
+
+                },
+                actions = {
+
+                }
+            )
+
+
+
+            NavigationRail(
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .background(color = Color.Gray.copy(alpha = 0.1f)),
+            ) {
+
+                NavigationRailItem(
+                    modifier = Modifier,
+                    icon = { Icon(imageVector = Icons.Filled.Favorite, contentDescription = "") },
+                    label = { Text("Favorites") },
+                    selected = false,
+                    onClick = {},
+                )
+
             }
         }
     }
--- composeApp/src/desktopMain/kotlin/com/github/klee0kai/cloud/main.kt
+++ composeApp/src/desktopMain/kotlin/com/github/klee0kai/cloud/main.kt
@@ -1,13 +1,20 @@
 package com.github.klee0kai.cloud
 
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "KotlinProject",
+        title = "CloudAnalytics",
     ) {
+        MenuBar {
+            Menu(text = "File", mnemonic = 'F') {
+                Item(text = "Save", onClick = {})
+            }
+        }
+
         App()
     }
 }
